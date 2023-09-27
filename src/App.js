dfff3b2f37ce8a8968b005f77574fb7d652da010
binoyam <binyam.techan01@gmail.com>
2023-09-28 02:02:14 +0300

    input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
-      <main>
-        <form>
-          <input type="text" placeholder='Create a new todo' />
+      <main className="main">
+        <form className="form">
+          <div className="input-div">
+            <input type="text" placeholder="Create a new todo..." />
+            <span className="circle"></span>
+          </div>
         </form>
+          <ul className="list">
+            <li>new item</li>
+            <li>new item</li>
+            <li>new item</li>
+            <li>new item</li>
+          </ul>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
