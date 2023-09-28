d2bf67d31a0ce689fff1b1d568da7761b3a93616
binoyam <binyam.techan01@gmail.com>
2023-09-29 01:23:56 +0300

    doing some research

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,33 +1,32 @@
 import React from "react";
 import crossIcon from "../images/icon-cross.svg";
-import { toBeChecked } from "@testing-library/jest-dom/matchers";
+
 
 function TodoList({ todos }) {
+
+
+
+
+
   console.log(todos);
   return (
     <ul className="list">
       {todos.map((todo, index) => {
         return (
-          <li 
-          className="todo"
-           key={index}>
-            <div>
-            <input 
-            className="checkbox"
-            type="checkbox"
-            title="Mark as complete"
-            toBeChecked
+          <li className="todo" key={index}>
+          <div>
+            <input
+              className="checkbox"
+              type="checkbox"
+              title="Mark as complete"
+              toBeChecked
             />
             {todo}
-            </div>
-            <button className="remove-btn">
-            <img 
-            src={crossIcon} 
-            alt="remove icon"
-            title="Remove-Task"
-            />
-            </button>
-          </li>
+          </div>
+          <button className="remove-btn">
+            <img src={crossIcon} alt="remove icon" title="Remove-Task" />
+          </button>
+        </li>
         );
       })}
     </ul>
